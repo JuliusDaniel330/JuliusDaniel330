f912fb13beb93ba4ec2ae3158fc0ddee3f974fef
JuliusDaniel330 <90117093+JuliusDaniel330@users.noreply.github.com>
2021-09-09 04:53:46 -0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+num1 = 20
+num2 = 30
+num3 = 40
+num4 =(num1+num2+num3)
+print(num4)
+
+
+firstname = "daniel" 
+secondname = "julius"
+print(firstname+secondname)
+
+
+
