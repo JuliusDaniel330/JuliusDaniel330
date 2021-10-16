10ece58f5660da7b9285ecb75a9aadf063ec5904
JuliusDaniel330 <90117093+JuliusDaniel330@users.noreply.github.com>
2021-10-16 07:30:39 -0700

    Add files via upload

---
+++ HTML,,DOCUM.py
@@ -0,0 +1,377 @@
+"""
+Tag	Description
+<!--...-->	Defines a comment
+<!DOCTYPE> 	Defines the document type
+<a>	Defines a hyperlink
+<abbr>	Defines an abbreviation or an acronym
+<acronym>	Not supported in HTML5. Use <abbr> instead.
+Defines an acronym
+<address>	Defines contact information for the author/owner of a document
+<applet>	Not supported in HTML5. Use <embed> or <object> instead.
+Defines an embedded applet
+<area>	Defines an area inside an image map
+<article>	Defines an article
+<aside>	Defines content aside from the page content
+<audio>	Defines embedded sound content
+<b>	Defines bold text
+<base>	Specifies the base URL/target for all relative URLs in a document
+<basefont>	Not supported in HTML5. Use CSS instead.
+Specifies a default color, size, and font for all text in a document
+<bdi>	Isolates a part of text that might be formatted in a different direction from other text outside it
+<bdo>	Overrides the current text direction
+<big>	Not supported in HTML5. Use CSS instead.
+Defines big text
+<blockquote>	Defines a section that is quoted from another source
+<body>	Defines the document's body
+<br>	Defines a single line break
+<button>	Defines a clickable button
+<canvas>	Used to draw graphics, on the fly, via scripting (usually JavaScript)
+<caption>	Defines a table caption
+<center>	Not supported in HTML5. Use CSS instead.
+Defines centered text
+<cite>	Defines the title of a work
+<code>	Defines a piece of computer code
+<col>	Specifies column properties for each column within a <colgroup> element
+<colgroup>	Specifies a group of one or more columns in a table for formatting
+<data>	Adds a machine-readable translation of a given content
+<datalist>	Specifies a list of pre-defined options for input controls
+<dd>	Defines a description/value of a term in a description list
+<del>	Defines text that has been deleted from a document
+<details>	Defines additional details that the user can view or hide
+<dfn>	Specifies a term that is going to be defined within the content
+<dialog>	Defines a dialog box or window
+<dir>	Not supported in HTML5. Use <ul> instead.
+Defines a directory list
+<div>	Defines a section in a document
+<dl>	Defines a description list
+<dt>	Defines a term/name in a description list
+<em>	Defines emphasized text
+<embed>	Defines a container for an external application
+<fieldset>	Groups related elements in a form
+<figcaption>	Defines a caption for a <figure> element
+<figure>	Specifies self-contained content
+<font>	Not supported in HTML5. Use CSS instead.
+Defines font, color, and size for text
+<footer>	Defines a footer for a document or section
+<form>	Defines an HTML form for user input
+<frame>	Not supported in HTML5.
+Defines a window (a frame) in a frameset
+<frameset>	Not supported in HTML5.
+Defines a set of frames
+<h1> to <h6>	Defines HTML headings
+<head>	Contains metadata/information for the document
+<header>	Defines a header for a document or section
+<hr>	Defines a thematic change in the content
+<html>	Defines the root of an HTML document
+<i>	Defines a part of text in an alternate voice or mood
+<iframe>	Defines an inline frame
+<img>	Defines an image
+<input>	Defines an input control
+<ins>	Defines a text that has been inserted into a document
+<kbd>	Defines keyboard input
+<label>	Defines a label for an <input> element
+<legend>	Defines a caption for a <fieldset> element
+<li>	Defines a list item
+<link>	Defines the relationship between a document and an external resource (most used to link to style sheets)
+<main>	Specifies the main content of a document
+<map>	Defines an image map
+<mark>	Defines marked/highlighted text
+<meta>	Defines metadata about an HTML document
+<meter>	Defines a scalar measurement within a known range (a gauge)
+<nav>	Defines navigation links
+<noframes>	Not supported in HTML5.
+Defines an alternate content for users that do not support frames
+<noscript>	Defines an alternate content for users that do not support client-side scripts
+<object>	Defines a container for an external application
+<ol>	Defines an ordered list
+<optgroup>	Defines a group of related options in a drop-down list
+<option>	Defines an option in a drop-down list
+<output>	Defines the result of a calculation
+<p>	Defines a paragraph
+<param>	Defines a parameter for an object
+<picture>	Defines a container for multiple image resources
+<pre>	Defines preformatted text
+<progress>	Represents the progress of a task
+<q>	Defines a short quotation
+<rp>	Defines what to show in browsers that do not support ruby annotations
+<rt>	Defines an explanation/pronunciation of characters (for East Asian typography)
+<ruby>	Defines a ruby annotation (for East Asian typography)
+<s>	Defines text that is no longer correct
+<samp>	Defines sample output from a computer program
+<script>	Defines a client-side script
+<section>	Defines a section in a document
+<select>	Defines a drop-down list
+<small>	Defines smaller text
+<source>	Defines multiple media resources for media elements (<video> and <audio>)
+<span>	Defines a section in a document
+<strike>	Not supported in HTML5. Use <del> or <s> instead.
+Defines strikethrough text
+<strong>	Defines important text
+<style>	Defines style information for a document
+<sub>	Defines subscripted text
+<summary>	Defines a visible heading for a <details> element
+<sup>	Defines superscripted text
+<svg>	Defines a container for SVG graphics
+<table>	Defines a table
+<tbody>	Groups the body content in a table
+<td>	Defines a cell in a table
+<template>	Defines a container for content that should be hidden when the page loads
+<textarea>	Defines a multiline input control (text area)
+<tfoot>	Groups the footer content in a table
+<th>	Defines a header cell in a table
+<thead>	Groups the header content in a table
+<time>	Defines a specific time (or datetime)
+<title>	Defines a title for the document
+<tr>	Defines a row in a table
+<track>	Defines text tracks for media elements (<video> and <audio>)
+<tt>	Not supported in HTML5. Use CSS instead.
+Defines teletype text
+<u>	Defines some text that is unarticulated and styled differently from normal text
+<ul>	Defines an unordered list
+<var>	Defines a variable
+<video>	Defines embedded video content
+<wbr>	Defines a possible line-break
+
+Basic HTML
+Tag	Description
+<!DOCTYPE> 	Defines the document type
+<html>	Defines an HTML document
+<head>	Contains metadata/information for the document
+<title>	Defines a title for the document
+<body>	Defines the document's body
+<h1> to <h6>	Defines HTML headings
+<p>	Defines a paragraph
+<br>	Inserts a single line break
+<hr>	Defines a thematic change in the content
+<!--...-->	Defines a comment
+Formatting
+Tag	Description
+<acronym>	Not supported in HTML5. Use <abbr> instead.
+Defines an acronym
+<abbr>	Defines an abbreviation or an acronym
+<address>	Defines contact information for the author/owner of a document/article
+<b>	Defines bold text
+<bdi>	Isolates a part of text that might be formatted in a different direction from other text outside it
+<bdo>	Overrides the current text direction
+<big>	Not supported in HTML5. Use CSS instead.
+Defines big text
+<blockquote>	Defines a section that is quoted from another source
+<center>	Not supported in HTML5. Use CSS instead.
+Defines centered text
+<cite>	Defines the title of a work
+<code>	Defines a piece of computer code
+<del>	Defines text that has been deleted from a document
+<dfn>	Specifies a term that is going to be defined within the content
+<em>	Defines emphasized text
+<font>	Not supported in HTML5. Use CSS instead.
+Defines font, color, and size for text
+<i>	Defines a part of text in an alternate voice or mood
+<ins>	Defines a text that has been inserted into a document
+<kbd>	Defines keyboard input
+<mark>	Defines marked/highlighted text
+<meter>	Defines a scalar measurement within a known range (a gauge)
+<pre>	Defines preformatted text
+<progress>	Represents the progress of a task
+<q>	Defines a short quotation
+<rp>	Defines what to show in browsers that do not support ruby annotations
+<rt>	Defines an explanation/pronunciation of characters (for East Asian typography)
+<ruby>	Defines a ruby annotation (for East Asian typography)
+<s>	Defines text that is no longer correct
+<samp>	Defines sample output from a computer program
+<small>	Defines smaller text
+<strike>	Not supported in HTML5. Use <del> or <s> instead.
+Defines strikethrough text
+<strong>	Defines important text
+<sub>	Defines subscripted text
+<sup>	Defines superscripted text
+<template>	Defines a container for content that should be hidden when the page loads
+<time>	Defines a specific time (or datetime)
+<tt>	Not supported in HTML5. Use CSS instead.
+Defines teletype text
+<u>	Defines some text that is unarticulated and styled differently from normal text
+<var>	Defines a variable
+<wbr>	Defines a possible line-break
+ADVERTISEMENT
+
+Forms and Input
+Tag	Description
+<form>	Defines an HTML form for user input
+<input>	Defines an input control
+<textarea>	Defines a multiline input control (text area)
+<button>	Defines a clickable button
+<select>	Defines a drop-down list
+<optgroup>	Defines a group of related options in a drop-down list
+<option>	Defines an option in a drop-down list
+<label>	Defines a label for an <input> element
+<fieldset>	Groups related elements in a form
+<legend>	Defines a caption for a <fieldset> element
+<datalist>	Specifies a list of pre-defined options for input controls
+<output>	Defines the result of a calculation
+Frames
+Tag	Description
+<frame>	Not supported in HTML5.
+Defines a window (a frame) in a frameset
+<frameset>	Not supported in HTML5.
+Defines a set of frames
+<noframes>	Not supported in HTML5.
+Defines an alternate content for users that do not support frames
+<iframe>	Defines an inline frame
+Images
+Tag	Description
+<img>	Defines an image
+<map>	Defines a client-side image map
+<area>	Defines an area inside an image map
+<canvas>	Used to draw graphics, on the fly, via scripting (usually JavaScript)
+<figcaption>	Defines a caption for a <figure> element
+<figure>	Specifies self-contained content
+<picture>	Defines a container for multiple image resources
+<svg>	Defines a container for SVG graphics
+Audio / Video
+Tag	Description
+<audio>	Defines sound content
+<source>	Defines multiple media resources for media elements (<video>, <audio> and <picture>)
+<track>	Defines text tracks for media elements (<video> and <audio>)
+<video>	Defines a video or movie
+Links
+Tag	Description
+<a>	Defines a hyperlink
+<link>	Defines the relationship between a document and an external resource (most used to link to style sheets)
+<nav>	Defines navigation links
+Lists
+Tag	Description
+<ul>	Defines an unordered list
+<ol>	Defines an ordered list
+<li>	Defines a list item
+<dir>	Not supported in HTML5. Use <ul> instead.
+Defines a directory list
+<dl>	Defines a description list
+<dt>	Defines a term/name in a description list
+<dd>	Defines a description of a term/name in a description list
+Tables
+Tag	Description
+<table>	Defines a table
+<caption>	Defines a table caption
+<th>	Defines a header cell in a table
+<tr>	Defines a row in a table
+<td>	Defines a cell in a table
+<thead>	Groups the header content in a table
+<tbody>	Groups the body content in a table
+<tfoot>	Groups the footer content in a table
+<col>	Specifies column properties for each column within a <colgroup> element
+<colgroup>	Specifies a group of one or more columns in a table for formatting
+Styles and Semantics
+Tag	Description
+<style>	Defines style information for a document
+<div>	Defines a section in a document
+<span>	Defines a section in a document
+<header>	Defines a header for a document or section
+<footer>	Defines a footer for a document or section
+<main>	Specifies the main content of a document
+<section>	Defines a section in a document
+<article>	Defines an article
+<aside>	Defines content aside from the page content
+<details>	Defines additional details that the user can view or hide
+<dialog>	Defines a dialog box or window
+<summary>	Defines a visible heading for a <details> element
+<data>	Adds a machine-readable translation of a given content
+Meta Info
+Tag	Description
+<head>	Defines information about the document
+<meta>	Defines metadata about an HTML document
+<base>	Specifies the base URL/target for all relative URLs in a document
+<basefont>	Not supported in HTML5. Use CSS instead.
+Specifies a default color, size, and font for all text in a document
+Programming
+Tag	Description
+<script>	Defines a client-side script
+<noscript>	Defines an alternate content for users that do not support client-side scripts
+<applet>	Not supported in HTML5. Use <embed> or <object> instead.
+Defines an embedded applet
+<embed>	Defines a container for an external (non-HTML) application
+<object>	Defines an embedded object
+<param>	Defines a parameter for an object
+
+HTML Attributes
+All HTML elements can have attributes
+Attributes provide additional information about elements
+Attributes are always specified in the start tag
+Attributes usually come in name/value pairs like: name="value"
+
+Chapter Summary
+All HTML elements can have attributes
+The href attribute of <a> specifies the URL of the page the link goes to
+The src attribute of <img> specifies the path to the image to be displayed
+The width and height attributes of <img> provide size information for images
+The alt attribute of <img> provides an alternate text for an image
+The style attribute is used to add styles to an element, such as color, font, size, and more
+The lang attribute of the <html> tag declares the language of the Web page
+The title attribute defines some extra information about an element
+
+Chapter Summary
+Use the style attribute for styling HTML elements
+Use background-color for background color
+Use color for text colors
+Use font-family for text fonts
+Use font-size for text sizes
+Use text-align for text alignment
+
+Use the HTML style attribute for inline styling
+Use the HTML <style> element to define internal CSS
+Use the HTML <link> element to refer to an external CSS file
+Use the HTML <head> element to store <style> and <link> elements
+Use the CSS color property for text colors
+Use the CSS font-family property for text fonts
+Use the CSS font-size property for text sizes
+Use the CSS border property for borders
+Use the CSS padding property for space inside the border
+Use the CSS margin property for space outside the border
+
+
+Here are some examples showing different LIKE operators with '%' and '_' wildcards:
+
+LIKE Operator	                                  Description
+WHERE CustomerName LIKE 'a%'	Finds any values that start with "a"
+WHERE CustomerName LIKE '%a'	Finds any values that end with "a"
+WHERE CustomerName LIKE '%or%'	Finds any values that have "or" in any position
+WHERE CustomerName LIKE '_r%'	Finds any values that have "r" in the second position
+WHERE CustomerName LIKE 'a_%'	Finds any values that start with "a" and are at least 2 characters in length
+WHERE CustomerName LIKE 'a__%'	Finds any values that start with "a" and are at least 3 characters in length
+WHERE ContactName LIKE 'a%o'	Finds any values that start with "a" and ends with "o"
+
+
+
+
+Wildcard Characters in MS Access
+Symbol	Description	Example
+*	Represents zero or more characters	bl* finds bl, black, blue, and blob
+?	Represents a single character	h?t finds hot, hat, and hit
+[]	Represents any single character within the brackets	h[oa]t finds hot and hat, but not hit
+!	Represents any character not in the brackets	h[!oa]t finds hit, but not hot and hat
+-	Represents any single character within the specified range	c[a-b]t finds cat and cbt
+#	Represents any single numeric character	2#5 finds 205, 215, 225, 235, 245, 255, 265, 275, 285, and 295
+Wildcard Characters in SQL Server
+Symbol	Description	Example
+%	Represents zero or more characters	bl% finds bl, black, blue, and blob
+_	Represents a single character	h_t finds hot, hat, and hit
+[]	Represents any single character within the brackets	h[oa]t finds hot and hat, but not hit
+^	Represents any character not in the brackets	h[^oa]t finds hit, but not hot and hat
+-	Represents any single character within the specified range	c[a-b]t finds cat and cbt
+All the wildcards can also be used in combinations!
+
+Here are some examples showing different LIKE operators with '%' and '_' wildcards:
+
+LIKE Operator	Description
+WHERE CustomerName LIKE 'a%'	Finds any values that starts with "a"
+WHERE CustomerName LIKE '%a'	Finds any values that ends with "a"
+WHERE CustomerName LIKE '%or%'	Finds any values that have "or" in any position
+WHERE CustomerName LIKE '_r%'	Finds any values that have "r" in the second position
+WHERE CustomerName LIKE 'a__%'	Finds any values that starts with "a" and are at least 3 characters in length
+WHERE ContactName LIKE 'a%o'	Finds any values that starts with "a" and ends with "o"
+
+"""
+
+
+
+
+
+
